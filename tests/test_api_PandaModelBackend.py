716cf7dd8d3aa6fe4590c72d68037473affcba56
Ketan Bhardwaj <ketanbj@gmail.com>
2025-03-04 10:46:56 -0500

    fixed module import in test

--- tests/test_api_PandaModelBackend.py
+++ tests/test_api_PandaModelBackend.py
@@ -1,7 +1,7 @@
 import unittest
 from grid2op.tests.aaa_test_backend_interface import AAATestBackendAPI
 
-import PandaModelsBackend
+from PandaModelsBackend import PandaModelsBackend
 
 class TestBackendAPI_PandaModelsBackend(AAATestBackendAPI, unittest.TestCase):
     def make_backend(self, detailed_infos_for_cascading_failures=False):
